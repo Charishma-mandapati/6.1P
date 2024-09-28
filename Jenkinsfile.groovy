2b6ba865590e54994f72722897ea8fbf4b10d956
Charishma-mandapati <mandapati.charishma97@gmail.com>
2024-09-28 20:24:48 +1000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building the project...'
-                bat 'npm install'
+                bat 'docker build -t node-js-sample .'
                 
 
             }
